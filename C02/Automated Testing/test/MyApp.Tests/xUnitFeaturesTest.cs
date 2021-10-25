34ca2467cd4f59b48171cc3983ad880ada704082
Carl-Hugo Marcotte <carlhugo@gmail.com>
2021-10-24 21:16:05 -0400

    Add AsyncFactTest class

--- C02/Automated Testing/test/MyApp.Tests/xUnitFeaturesTest.cs
+++ C02/Automated Testing/test/MyApp.Tests/xUnitFeaturesTest.cs
@@ -21,6 +21,63 @@ namespace MyApp
             }
         }
 
+        public class AsyncFactTest
+        {
+            [Fact]
+            public async Task Should_be_equal()
+            {
+                var expectedValue = 2;
+                var actualValue = 2;
+                await Task.Yield(); // Async operation
+                Assert.Equal(expectedValue, actualValue);
+            }
+        }
+
+        public class AssertionTest
+        {
+            [Fact]
+            public void Exploring_xUnit_assertions()
+            {
+                object obj1 = new MyClass { Name = "Object 1" };
+                object obj2 = new MyClass { Name = "Object 1" };
+                object obj3 = obj1;
+                object obj4 = default(MyClass);
+
+                Assert.Equal(expected: 2, actual: 2);
+                Assert.NotEqual(expected: 2, actual: 1);
+
+                Assert.Same(obj1, obj3);
+                Assert.NotSame(obj1, obj2);
+                Assert.Equal(obj1, obj2);
+
+                Assert.Null(obj4);
+                Assert.NotNull(obj3);
+
+                var instanceOfMyClass = Assert.IsType<MyClass>(obj1);
+                Assert.Equal(expected: "Object 1", actual: instanceOfMyClass.Name);
+
+                var exception = Assert.Throws<SomeCustomException>(
+                    () => OperationThatThrows("Toto")
+                );
+                Assert.Equal(expected: "Toto", actual: exception.Name);
+
+                static void OperationThatThrows(string name)
+                {
+                    throw new SomeCustomException { Name = name };
+                }
+            }
+
+            private record class MyClass
+            {
+                public string Name { get; set; }
+            }
+
+            private class SomeCustomException : Exception
+            {
+                public string Name { get; set; }
+            }
+        }
+
         public class InlineDataTest
         {
             [Theory]

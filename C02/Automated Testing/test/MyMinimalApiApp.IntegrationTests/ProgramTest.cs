11243c938f75a61502cc872fa4eb87d806a88532
Carl-Hugo Marcotte <carlhugo@gmail.com>
2021-10-24 23:39:18 -0400

    Add minimal API program and integration tests

---
+++ C02/Automated Testing/test/MyMinimalApiApp.IntegrationTests/ProgramTest.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyMinimalApiApp.IntegrationTests
+{
+    public class ProgramTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProgramTest(WebApplicationFactory<Program> webApplicationFactory)
+        {
+            _httpClient = webApplicationFactory.CreateClient();
+        }
+
+        public class Get : ProgramTest
+        {
+            public Get(WebApplicationFactory<Program> webApplicationFactory) : base(webApplicationFactory) { }
+
+            [Fact]
+            public async Task Should_respond_a_status_200_OK()
+            {
+                // Act
+                var result = await _httpClient.GetAsync("/");
+
+                // Assert
+                Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+            }
+
+            [Fact]
+            public async Task Should_respond_the_expected_strings()
+            {
+                // Act
+                var result = await _httpClient.GetAsync("/");
+
+                // Assert
+                var contentText = await result.Content.ReadAsStringAsync();
+                Assert.Equal("Hello World!", contentText);
+            }
+        }
+    }
+}

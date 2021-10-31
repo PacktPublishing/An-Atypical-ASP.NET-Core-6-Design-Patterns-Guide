1d1e30b3981dbb7aaf6ea89785925b4199fb7cde
Carl-Hugo Marcotte <carlhugo@gmail.com>
2021-10-31 16:43:11 -0400

    Add program to NinjaBeforeOCP and set ports

---
+++ C03/Open Close/NinjaBeforeOCP/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using NinjaBeforeOCP;
+using System;
+using System.Numerics;
+using System.Threading.Tasks;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+app.MapGet("/", async (HttpContext context) =>
+{
+    // Create actors
+    var target = new Ninja("The Unseen Mirage");
+    var ninja = new Ninja("The Blue Phantom");
+
+    // First attack (Sword)
+    var result = ninja.Attack(target);
+    await PrintAttackResult(result);
+
+    // Second attack (Shuriken)
+    var result2 = ninja.Attack(target);
+    await PrintAttackResult(result2);
+
+    // Write the outcome of an AttackResult to response stream
+    async Task PrintAttackResult(AttackResult attackResult)
+    {
+        await context.Response.WriteAsync($"'{attackResult.Attacker}' attacked '{attackResult.Target}' using '{attackResult.Weapon}'!{Environment.NewLine}");
+    }
+});
+app.Run();

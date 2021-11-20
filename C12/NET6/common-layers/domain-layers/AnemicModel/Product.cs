d3efddd9c943f658517e60288b11dea85c99042f
Carl-Hugo Marcotte <carlhugo@gmail.com>
2021-11-20 15:30:54 -0500

    Add domain layers light sample

---
+++ C12/NET6/common-layers/domain-layers/AnemicModel/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace DomainLayers.AnemicModel;
+
+public class Product
+{
+    public int? Id { get; set; }
+    public string? Name { get; set; }
+    public int QuantityInStock { get; set; }
+}
---
+++ C12/NET6/common-layers/domain-layers/Shared/NegativeValueException.cs
@@ -0,0 +1,10 @@
+﻿namespace DomainLayers;
+
+public class NegativeValueException : Exception
+{
+    public NegativeValueException(int amountToAddOrRevove)
+        : base($"The amount to add or remove can't be negative. Provided: {amountToAddOrRevove}.")
+    {
+
+    }
+}

9157ba48184151e78dae4f0b03e2cfafe1d036d9
Carl-Hugo Marcotte <carlhugo@gmail.com>
2021-11-20 20:56:32 -0500

    Make DeleteAsync in line with abstract-layer

--- C12/NET6/common-layers/data-layer/IRepository.cs
+++ C12/NET6/common-layers/data-layer/IRepository.cs
@@ -13,7 +13,7 @@ namespace DataLayer
         Task<T?> GetByIdAsync(TId id, CancellationToken cancellationToken);
         Task<T> CreateAsync(T entity, CancellationToken cancellationToken);
         Task UpdateAsync(T entity, CancellationToken cancellationToken);
-        Task DeleteAsync(T entity, CancellationToken cancellationToken);
+        Task DeleteAsync(TId id, CancellationToken cancellationToken);
     }
 
     public interface IEntity<TId>

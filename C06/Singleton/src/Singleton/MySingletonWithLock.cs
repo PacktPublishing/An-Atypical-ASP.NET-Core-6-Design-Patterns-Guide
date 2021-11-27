85605c1c2fb8abe06be2049f468d01f245896de1
Carl-Hugo Marcotte <carlhugo@gmail.com>
2021-11-27 09:46:45 -0500

    Add nullable reference types

--- C06/Singleton/src/Singleton/MySingletonWithLock.cs
+++ C06/Singleton/src/Singleton/MySingletonWithLock.cs
@@ -2,16 +2,16 @@
 {
     public class MySingletonWithLock
     {
-        private readonly static object _myLock = new object();
+        private readonly static object _myLock = new();
 
-        private static MySingletonWithLock _instance;
+        private static MySingletonWithLock? _instance;
         private MySingletonWithLock() { }
 
         public static MySingletonWithLock Create()
         {
             lock (_myLock)
             {
-                if (_instance == default(MySingletonWithLock))
+                if (_instance == default)
                 {
                     _instance = new MySingletonWithLock();
                 }

6c312dd89dba44f2ea02b73d948a4935e00e2b55
Carl-Hugo Marcotte <carlhugo@gmail.com>
2021-11-28 17:38:44 -0500

    Add an endpoints menu

--- C07/ServiceLocator/Program.cs
+++ C07/ServiceLocator/Program.cs
@@ -7,7 +7,13 @@ builder.Services
 ;
 var app = builder.Build();
 app.MapControllers();
-app.MapGet("minimal-api", (IMyService myService) =>
+app.MapGet("/", (HttpContext context) => new[] {
+    $"https://{context.Request.Host}/service-locator",
+    $"https://{context.Request.Host}/method-injection",
+    $"https://{context.Request.Host}/constructor-injection",
+    $"https://{context.Request.Host}/minimal-api",
+});
+app.MapGet("/minimal-api", (IMyService myService) =>
 {
     myService.Execute();
     return "Success!";

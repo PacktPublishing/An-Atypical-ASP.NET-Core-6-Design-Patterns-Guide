313e719d3dee2dcf7567046849bc0a2354a8b6c7
Carl-Hugo Marcotte <carlhugo@gmail.com>
2021-12-05 15:55:52 -0500

    Add Should_log_the_Service_Execute_line_using_WebApplication

--- C08/Logging/BaseAbstractions.cs
+++ C08/Logging/BaseAbstractions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -25,6 +26,27 @@ public class BaseAbstractions
             })
             .Build();
         var service = host.Services.GetRequiredService<IService>();
+
+        // Act
+        service.Execute();
+
+        // Assert
+        Assert.Collection(lines,
+            line => Assert.Equal("Service.Execute()", line)
+        );
+    }
+
+    [Fact]
+    public void Should_log_the_Service_Execute_line_using_WebApplication()
+    {
+        // Arrange
+        var lines = new List<string>();
+        var builder = WebApplication.CreateBuilder();
+        builder.Logging.ClearProviders()
+            .AddAssertableLogger(lines);
+        builder.Services.AddSingleton<IService, Service>();
+        var app = builder.Build();
+        var service = app.Services.GetRequiredService<IService>();
 
         // Act
         service.Execute();

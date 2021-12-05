c2c86dc4266ea14e217e19a2508c94383992fa00
Carl-Hugo Marcotte <carlhugo@gmail.com>
2021-12-04 19:57:09 -0500

    Simplify service registration

--- C08/OptionsConfiguration/Program.cs
+++ C08/OptionsConfiguration/Program.cs
@@ -2,25 +2,19 @@ using OptionsConfiguration;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.Configure<ConfigureMeOptions>(builder.Configuration.GetSection("configureMe"));
-builder.Services.AddSingleton<IConfigureOptions<ConfigureMeOptions>, Configure1ConfigureMeOptions>();
-builder.Services.AddSingleton<IConfigureOptions<ConfigureMeOptions>, Configure2ConfigureMeOptions>();
-builder.Services.Configure<ConfigureMeOptions>(options =>
-{
-    options.Lines = options.Lines.Append("Another Configure call");
-});
-builder.Services.PostConfigure<ConfigureMeOptions>(options =>
-{
-    options.Lines = options.Lines.Append("What about PostConfigure?");
-});
-builder.Services.PostConfigureAll<ConfigureMeOptions>(options =>
-{
-    options.Lines = options.Lines.Append("Did you forgot about PostConfigureAll?");
-});
-builder.Services.ConfigureAll<ConfigureMeOptions>(options =>
-{
-    options.Lines = options.Lines.Append("Or ConfigureAll?");
-});
+builder.Services
+    .Configure<ConfigureMeOptions>(builder.Configuration.GetSection("configureMe"))
+    .AddSingleton<IConfigureOptions<ConfigureMeOptions>, Configure1ConfigureMeOptions>()
+    .AddSingleton<IConfigureOptions<ConfigureMeOptions>, Configure2ConfigureMeOptions>()
+;
+builder.Services.Configure<ConfigureMeOptions>(options
+    => options.Lines = options.Lines.Append("Another Configure call"));
+builder.Services.PostConfigure<ConfigureMeOptions>(options
+    => options.Lines = options.Lines.Append("What about PostConfigure?"));
+builder.Services.PostConfigureAll<ConfigureMeOptions>(options
+    => options.Lines = options.Lines.Append("Did you forgot about PostConfigureAll?"));
+builder.Services.ConfigureAll<ConfigureMeOptions>(options
+    => options.Lines = options.Lines.Append("Or ConfigureAll?"));
 builder.Services.AddOptions<ConfigureMeOptions>().Validate(options =>
 {
     options.Lines = options.Lines.Append("Validate was not intended for this, but this is a trace isn't it?");
@@ -29,7 +23,7 @@ builder.Services.AddOptions<ConfigureMeOptions>().Validate(options =>
 
 var app = builder.Build();
 app.MapGet("/", (HttpContext context) => new[] { $"https://{context.Request.Host}/configure-me" });
-app.MapGet("/configure-me", (IOptionsMonitor<ConfigureMeOptions> options) => options.CurrentValue);
+app.MapGet("/configure-me", (IOptionsMonitor<ConfigureMeOptions> options) => options);
 app.Run();
 
 public class Configure1ConfigureMeOptions : IConfigureOptions<ConfigureMeOptions>

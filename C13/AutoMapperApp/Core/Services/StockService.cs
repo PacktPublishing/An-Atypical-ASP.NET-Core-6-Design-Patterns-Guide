43021e9a31d6106729d6b37ae15af299a2ea0d57
Carl-Hugo Marcotte <carlhugo@gmail.com>
2021-12-10 16:52:50 -0500

    Add AutoMapperApp

---
+++ C13/AutoMapperApp/Core/Services/StockService.cs
@@ -0,0 +1,38 @@
+﻿using Core.Repositories;
+
+namespace Core.Services;
+
+public class StockService
+{
+    private readonly IProductRepository _repository;
+    public StockService(IProductRepository repository)
+    {
+        _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+    }
+
+    public async Task<int> AddStockAsync(int productId, int amount, CancellationToken cancellationToken)
+    {
+        var product = await _repository.FindByIdAsync(productId, cancellationToken);
+        if (product == null)
+        {
+            throw new ProductNotFoundException(productId);
+        }
+        product.AddStock(amount);
+        await _repository.UpdateAsync(product, cancellationToken);
+
+        return product.QuantityInStock;
+    }
+
+    public async Task<int> RemoveStockAsync(int productId, int amount, CancellationToken cancellationToken)
+    {
+        var product = await _repository.FindByIdAsync(productId, cancellationToken);
+        if (product == null)
+        {
+            throw new ProductNotFoundException(productId);
+        }
+        product.RemoveStock(amount);
+        await _repository.UpdateAsync(product, cancellationToken);
+
+        return product.QuantityInStock;
+    }
+}

74a33b89a3bcfd25ab100cc90051bf58fdad1891
Carl-Hugo Marcotte <carlhugo@gmail.com>
2022-01-08 16:13:10 -0500

    Add root "menu"

--- C09/src/Facade/Program.cs
+++ C09/src/Facade/Program.cs
@@ -6,12 +6,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services
     .AddOpaqueFacadeSubSystem()
     .AddTransparentFacadeSubSystem()
-    .AddSingleton<IComponentB, UpdatedComponentB>()
+    //.AddSingleton<IComponentB, UpdatedComponentB>()
 ;
 
 var app = builder.Build();
-app.MapGet("/opaque/a", (IOpaqueFacade opaqueFacade) => opaqueFacade.ExecuteOperationA());
-app.MapGet("/opaque/b", (IOpaqueFacade opaqueFacade) => opaqueFacade.ExecuteOperationB());
-app.MapGet("/transparent/a", (ITransparentFacade transparentFacade) => transparentFacade.ExecuteOperationA());
-app.MapGet("/transparent/b", (ITransparentFacade transparentFacade) => transparentFacade.ExecuteOperationB());
+app.MapGet("/", (HttpContext context) =>
+new[] {
+    $"https://{context.Request.Host}/opaque/a",
+    $"https://{context.Request.Host}/opaque/b",
+    $"https://{context.Request.Host}/transparent/a",
+    $"https://{context.Request.Host}/transparent/b"
+});
+app.MapGet("/opaque/a", (IOpaqueFacade opaqueFacade)
+    => opaqueFacade.ExecuteOperationA());
+app.MapGet("/opaque/b", (IOpaqueFacade opaqueFacade)
+    => opaqueFacade.ExecuteOperationB());
+app.MapGet("/transparent/a", (ITransparentFacade transparentFacade)
+    => transparentFacade.ExecuteOperationA());
+app.MapGet("/transparent/b", (ITransparentFacade transparentFacade)
+    => transparentFacade.ExecuteOperationB());
 app.Run();

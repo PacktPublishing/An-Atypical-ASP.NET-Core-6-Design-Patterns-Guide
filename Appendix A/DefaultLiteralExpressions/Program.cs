f4cd566fee924aec93e181d13f26798106401d35
Carl-Hugo Marcotte <carlhugo@gmail.com>
2022-01-08 11:39:39 -0500

    Fix build and nullable references

--- Appendix A/DefaultLiteralExpressions/Program.cs
+++ Appendix A/DefaultLiteralExpressions/Program.cs
@@ -1,9 +1,11 @@
-﻿public class DefaultLiteralExpression<T>
+﻿Console.Write("See class bellow...");
+
+public class DefaultLiteralExpression<T>
 {
     public void Execute()
     {
         // Initialize a variable to its default value
-        T myVariable = default;
+        T? myVariable = default;
 
         var defaultResult1 = SomeMethod();
 
@@ -12,13 +14,13 @@
     }
 
     // Set the default value of an optional method parameter
-    public object SomeMethod(T input = default)
+    public object? SomeMethod(T? input = default)
     {
         // Return a default value in a return statement
         return default;
     }
 
     // Return a default value in an expression-bodied member
-    public object SomeOtherMethod(T input, int i) => default;
+    public object? SomeOtherMethod(T? input, int i) => default;
 }
 

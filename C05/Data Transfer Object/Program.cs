53a3ae485f68347f91011bf02a45b3dbf6def338
Carl-Hugo Marcotte <carlhugo@gmail.com>
2022-01-16 10:53:33 -0500

    Add OpenAPI/Swagger

--- C05/Data Transfer Object/Program.cs
+++ C05/Data Transfer Object/Program.cs
@@ -1,9 +1,14 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.UseDeveloperExceptionPage();
 }
 else

cd77fe3c425e62115302fb4e3b17da41fac8dcca
Carl-Hugo Marcotte <carlhugo@gmail.com>
2022-01-22 19:40:20 -0500

    Add missing info from .NET 6 template

--- C18/WASM/Program.cs
+++ C18/WASM/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using StateR;
 using StateR.Blazor.ReduxDevTools;
@@ -10,6 +11,7 @@ public class Program
     {
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
         builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 

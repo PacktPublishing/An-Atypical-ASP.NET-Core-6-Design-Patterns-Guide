e7b9dbf9dba3a793254d93d3e30328d003a33bdf
Carl-Hugo Marcotte <carlhugo@gmail.com>
2022-02-05 11:58:42 -0500

    Update to minimal hosting model

--- C17/src/PageController/Program.cs
+++ C17/src/PageController/Program.cs
@@ -1,16 +1,40 @@
-﻿namespace PageController;
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using PageController.Data;
+using PageController.TagHelpers;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<EmployeeDbContext>(
+    options => options.UseInMemoryDatabase(nameof(EmployeeDbContext))
+);
+builder.Services.AddRazorPages();
+builder.Services
+    .Configure<RssFeedTagHelperComponentOptions>(builder.Configuration.GetSection("RssFeed"))
+    .AddTransient(sp => sp.GetRequiredService<IOptionsMonitor<RssFeedTagHelperComponentOptions>>().CurrentValue)
+    .AddTransient<ITagHelperComponent, RssFeedTagHelperComponent>()
+    .AddTransient<ITagHelperComponent, MinifierTagHelperComponent>()
+;
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
--- C17/src/TransformTemplateView/Program.cs
+++ C17/src/TransformTemplateView/Program.cs
@@ -1,16 +1,35 @@
-﻿namespace TransformTemplateView;
+﻿using Microsoft.EntityFrameworkCore;
+using TransformTemplateView.Data;
+using TransformTemplateView.Services;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services
+    .AddSingleton<ICorporationFactory, DefaultCorporationFactory>()
+    .AddDbContext<EmployeeDbContext>(
+        options => options.UseInMemoryDatabase(nameof(EmployeeDbContext))
+    )
+    .AddRazorPages()
+;
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
